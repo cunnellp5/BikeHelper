73d8267c53ee960b0500e726e5b2b1550ce3274d
cunnellp5 <philip.cunnell@colorado.edu>
2017-03-15 09:35:37 -0600

    cap day 3

--- src/containers/bicyclist.vue
+++ src/containers/bicyclist.vue
@@ -44,7 +44,6 @@ export default {
 </script>
 
 
-
 <style lang="css">
   #bicyclist {
     display: flex;
--- src/containers/home.vue
+++ src/containers/home.vue
@@ -1,12 +1,22 @@
 <template lang="html">
-  <div id="home">
+  <div id="home" class="content">
     <router-link class="nav-item is-tab" to="/bicyclist">
       <i class="fa fa-bicyclist"></i> Bicyclist
     </router-link>
     <router-link class="nav-item is-tab" to="/projects">
       <i class="fa fa-list-alt"></i> Bicycle Store
     </router-link>
-    <h1 class="is-large">About:</h1>
+    <h1>About:</h1>
+    <ul>
+      <li>Sign in as a bicyclist to get help from bike stores.</li>
+        <ul>
+          <li>Send out a help notification and some info to recieve help</li>
+        </ul>
+      <li>Bike stores may sign in to view those in need.</li>
+      <ul>
+        <li>Bike stores can then rescue you.</li>
+      </ul>
+    </ul>
   </div>
 </template>
 
@@ -17,4 +27,7 @@ export default {
 </script>
 
 <style lang="css">
+  .borderMe {
+    margin-bottom: 20px;
+  }
 </style>
